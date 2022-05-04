2fcd46822385a7dff7b1a3a853afae6413d2ea17
Jonathan Berroa <berroa-jonatan@outlook.es>
2022-05-04 10:02:32 -0500

    cambios en pagina azure

---
+++ admin/config.php
@@ -0,0 +1,2 @@
+<?php $username2 = "kenyi"; // usuario
+$password2 = "123456"; // contraseña ?>
---
+++ integrantes.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>AZUREBOT - AZURE TEAM</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/az.jpeg" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   </head>
+   <!-- body -->
+   <body class="main-layout">
+      <!-- loader  -->
+      <div class="loader_bg">
+         <div class="loader"><img src="images/loading1.gif" alt="#" /></div>
+      </div>
+      <!-- end loader -->
+      <!-- header -->
+      <header>
+         <!-- header inner -->
+         <div class="header">
+            <div class="container-fluid">
+               <div class="row">
+                  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                     <div class="full">
+                        <div class="center-desk">
+                           <div class="logo">
+                           <a href="index.php"><img src="images/logo.jpeg" alt="#" /></a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                     <div class="header_information">
+                        <nav class="navigation navbar navbar-expand-md navbar-dark ">
+                           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                           <span class="navbar-toggler-icon"></span>
+                           </button>
+                           <div class="collapse navbar-collapse" id="navbarsExample04">
+                              <ul class="navbar-nav mr-auto">
+                                 <li class="nav-item">
+                                    <a class="nav-link" href="index.php"> INICIO  </a>
+                                 </li>
+                                 <li class="nav-item">
+                                    <a class="nav-link" href="nosotros.php"> NOSOTROS  </a>
+                                 </li> 
+                                 <li class="nav-item active">
+                                    <a class="nav-link" href="integrantes.php">INTEGRANTES</a>
+                                 </li>
+                                 <li class="nav-item">
+                                    <a class="nav-link" href="iot.php">IOT</a>
+                                 </li>
+                                 <li class="nav-item">
+                                    <a class="nav-link" href="chatbot.php">CHATBOT</a>
+                                 </li>
+                                 <li class="nav-item">
+                                    <a class="nav-link" href="realidad.php">R.V.</a>
+                                 </li>
+                              </ul>
+                              <div class="sign_btn"><a href="admin/login.php">Iniciar Sesión</a></div>
+                           </div>
+                        </nav>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </header>
+      <!-- end header inner -->
+      <!-- end header -->
+      <div class="ourwork">
+          <div class="container">
+             <div class="row">
+                <div class="col-md-12">
+                   <div class="titlepage">
+                      <h2>LOS INTEGRANTES DE TEAM AZURE</h2>
+                   </div>
+                </div>
+             </div>
+
+          <div class="row">
+            <div class="col-md-3"> 
+            </div>
+                <div class="col-md-3">
+                   <div class="our_box">
+                     <!-- integrante -->
+                     <div class="jonu">
+                     <strong>LIDER</strong>
+                        <img src="images/kenyi.jpeg" class="rounded-circle" alt="#"/>
+                           <h3>Kenyi Gonzalo Choque Pino</h3><br>
+                           <a class="read_more" href="kenyi.php">Ver perfil</a>
+                     </div>
+                     <!-- fin integrante -->
+                   </div>
+                </div>
+                <div class="col-md-3">
+                   <div class="our_box">
+                      <!-- integrante -->
+                     <div class="jonu">
+                     <strong>QA</strong>
+                        <img src="images/jess.jpeg" class="rounded-circle" alt="#"/>
+                           <h3>Katherine Jessica Huaraca Torres</h3><br>
+                  
+                           <a class="read_more" href="jess.php">Ver perfil</a>
+                     </div>
+                     <!-- fin integrante -->
+                   </div>
+                </div>
+                <div class="col-md-3">
+                 </div>
+
+
+
+                <div class="col-md-3">
+                   <div class="our_box">
+                       <!-- integrante -->
+                     <div class="jonu">
+                     <strong>Encargado FullStack de <br>IOT</strong>
+                        <img src="images/diego.jpeg" class="rounded-circle" alt="#"/>
+                           <h3>Diego Axel Arce Muñoz</h3><br>
+                          
+                           <a class="read_more" href="diego.php">Ver perfil</a>
+                     </div>
+                     <!-- fin integrante -->
+                   </div>
+                </div>
+                <div class="col-md-6">
+                   <div class="our_box">
+                        <!-- integrante -->
+                     <div class="jonu">
+                        <img src="images/AZUREBOT.jpeg" class="rounded-circle" alt="#"/>
+
+                     </div>
+                     <!-- fin integrante -->
+                   </div>
+                </div>
+               
+                <div class="col-md-3">
+                   <div class="our_box">
+                        <!-- integrante -->
+                     <div class="jonu">
+                     <strong>Encargado FullStack Gemelos Digitales</strong>
+                        <img src="images/joel.jpeg" class="rounded-circle" alt="#"/>
+                           <h3>Christian Joel Cruz Gonzales</h3><br>
+                           
+                           <a class="read_more" href="joel.php">Ver perfil</a>
+                     </div>
+                     <!-- fin integrante -->
+                   </div>
+                </div>
+
+
+                <div class="col-md-3"> 
+            </div>
+        <div class="col-md-3">
+                   <div class="our_box">
+                        <!-- integrante -->
+                     <div class="jonu">
+                     <strong>Encargado FullStack de NPL Chatbot</strong>
+                        <img src="images/jony.jpeg" class="rounded-circle" alt="#"/>
+                           <h3>Jonathan Christian Berroa Atayupanqui</h3><br>
+                         
+                           <a class="read_more" href="jony.php">Ver perfil</a>
+                     </div>
+                     <!-- fin integrante -->
+                   </div>
+                </div>
+                <div class="col-md-3">
+                   <div class="our_box">
+                       <!-- integrante -->
+                     <div class="jonu">
+                     <strong>Encargado FullStack de Realidad Virtual </strong>
+                        <img src="images/elvis.jpeg" class="rounded-circle" alt="#"/>
+                           <h3>Elvis Armando Condori Gutierrez</h3><br>
+                       
+                           <a class="read_more" href="elvis.php">Ver perfil</a>
+                     </div>
+                     <!-- fin integrante -->
+                   </div>
+                </div>
+                <div class="col-md-3">
+                 </div>
+             </div>
+          </div>
+       </div>
+     
+      
+      
+      <!--  footer -->
+      <footer>
+            <div class="copyright">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-md-12">
+                        <p>© 2022 Todos los derechos reservados - Team Azure</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </footer>
+      <!-- end footer -->
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+    
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+   </body>
+</html>
+
